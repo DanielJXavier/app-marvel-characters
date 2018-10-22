cfc13324b695ce94221c7eedba306c00048e90d5
Daniel Xavier <dxdanielxavier@gmail.com>
2018-10-22 13:19:46 -0300

    Add Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,17 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
 
-import { updateFilter, fetchCharacters } from '../actions'
-
+import Header from './Header'
 import ListCharacters from './ListCharacters'
-
-import './App.sass'
-import search from '../assets/images/search.svg'
 
 class App extends Component {
   render() {
-    const { updateFilter, fetchCharacters } = this.props
-
     return (
       <section>
-        <div className="header">
-          <h1 className="title">Marvel Characters</h1>
-          <div className="search">
-            <input type="text" placeholder="Search characters by name" onChange={(e) => updateFilter(e.target.value)} onKeyDown={(e) => e.keyCode === 13 && fetchCharacters(true)} />
-            <button><img src={search} alt="Edit icon" onClick={() => fetchCharacters(true)} /></button>
-          </div>
-        </div>
+        <Header />
         <ListCharacters />
       </section>
     )
   }
 }
 
-const mapStateToProps = (state) => ({})
-
-const mapDispatchToProps = (dispatch) => ({
-  updateFilter: (filter) => dispatch(updateFilter(filter)),
-  fetchCharacters: (reset) => dispatch(fetchCharacters(reset))
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import { updateFilter, fetchCharacters } from '../actions'
+
+import './Header.sass'
+import search from '../assets/images/search.svg'
+
+class Header extends Component {
+  render() {
+    const { filter } = this.props
+    const { updateFilter, fetchCharacters } = this.props
+
+    return (
+      <div className="header">
+        <h1 className="title">Marvel Characters</h1>
+        <div className="search">
+          <input type="text" placeholder="Search characters by name" value={filter} onChange={(e) => updateFilter(e.target.value)} onKeyDown={(e) => e.keyCode === 13 && fetchCharacters(true)} />
+          <button><img src={search} alt="Edit icon" onClick={() => fetchCharacters(true)} /></button>
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => ({
+  filter: state.filter.filter
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  updateFilter: (filter) => dispatch(updateFilter(filter)),
+  fetchCharacters: (reset) => dispatch(fetchCharacters(reset))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header)
