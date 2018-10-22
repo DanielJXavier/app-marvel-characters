0b768da77dff1aa98a91be7d9c5c67142fcada60
Daniel Xavier <dxdanielxavier@gmail.com>
2018-10-21 21:17:46 -0300

    Add header

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,13 @@ import search from '../assets/images/search.svg'
 
 export default () => (
   <main>
+    <div className="header">
+      <h1 className="title">Marvel Characters</h1>
+      <div className="search">
+        <input type="text" placeholder="Search characters" />
+        <button><img src={search} alt="Edit icon" /></button>
+      </div>
+    </div>
     <ListCharacters />
   </main>
 )
